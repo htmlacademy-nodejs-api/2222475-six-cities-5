010aa645f2ddde0e6ede1e1a474bb4617a8c1fc0
gerasimchikmi <gerasimchikmi@iek.ru>
2023-10-20 17:10:02 +0300

    Завершаем проектировать базу данных

--- src/shared/modules/offer/offer-service.interface.ts
+++ src/shared/modules/offer/offer-service.interface.ts
@@ -1,9 +1,15 @@
 import { CreateOfferDto } from './dto/create-offer.dto.js';
 import { DocumentType } from '@typegoose/typegoose';
 import { OfferEntity } from './offer.entity.js';
+import { UpdateOfferDto } from './dto/update-offer.dto.js';
 
 export interface OfferService {
   create(dto: CreateOfferDto): Promise<DocumentType<OfferEntity>>;
   findById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  find(): Promise<DocumentType<OfferEntity>[]>;
+  deleteById(offerId: string): Promise<DocumentType<OfferEntity> | null>;
+  updateById(offerId: string, dto: UpdateOfferDto): Promise<DocumentType<OfferEntity> | null>;
+  findPremium(): Promise<DocumentType<OfferEntity>[]>;
+  incCommentCount(offerId: string): Promise<DocumentType<OfferEntity> | null>;
 }
 
